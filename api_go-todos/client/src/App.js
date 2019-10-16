6040f38030d52fa79f9bd9e5a1f06186ea8000c2
Al-un <alun.sng@gmail.com>
2019-10-16 13:38:47 +0900

    Adding React client to go-todos

---
+++ api_go-todos/client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import {Container} from "semantic-ui-react"
+
+import TodoList from "./components/TodoList"
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <TodoList />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
